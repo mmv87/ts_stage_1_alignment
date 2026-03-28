b48d43e9912a60292a67eae1378e2216f8cf7b8c
mithun_vmk <mithun.kct@gmail.com>
2026-03-27 20:54:28 -0400

    small

--- ts_dataloader.py
+++ ts_dataloader.py
@@ -10,15 +10,13 @@ import torch
 import json
 from transformers import AutoModelForCausalLM,AutoTokenizer
 device ='cuda' if torch.cuda.is_available() else 'cpu'
-
+"""
 abs_modelpath="D:/hf_cache/hub/models--microsoft--Phi-4-mini-reasoning/snapshots/0e3b1e2d02ee478a3743abe3f629e9c0cb722e0a"
 ##print('path_read')
 os.environ["TRANSFORMERS_OFFLINE"] = "1"
 os.environ["HF_HUB_OFFLINE"] = "1"
-
 model_name='./hub/microsoft/phi-4-mini-reasoning'
 device ='cpu'
-
 print(device)
 model=AutoModelForCausalLM.from_pretrained(abs_modelpath,local_files_only=True)
 model.to(device)
@@ -28,9 +26,8 @@ tokenized = tokenizer(input_text,return_tensors='pt',add_special_tokens=False)['
 ###add special_tokens to the tokenizer
 special_token_dict={'pad_token':"<|pad|>","additional_special_tokens":['<ts>','<ts/>']}
 tokenizer.add_special_tokens(special_token_dict)
-
 align_256_file='D:/Doctoral_research/code_implementation/Time_series_reasoning/align_256.jsonl'
-ift_file='D:/Doctoral_research/code_implementation/Time_series_reasoning/ift.jsonl'
+ift_file='D:/Doctoral_research/code_implementation/Time_series_reasoning/ift.jsonl'"""
 ##print(align_256_file)
 
 ## Dataset class to get the pipeline for a sample
@@ -292,9 +289,10 @@ def collate_func(batch,tokenizer=None):
 dataset_for_test=ts_textual(128,128,tokenizer,align_256_file,device=device)
 dataloader=DataLoader(dataset_for_test,batch_size=1,shuffle=True,collate_fn=lambda b:collate_func(b,tokenizer=tokenizer))
 
+"""
 for batch in dataloader:
-  """print(batch['input_ids'].shape)
-  print(batch['attention_mask'].shape)"""
+  print(batch['input_ids'].shape)
+  print(batch['attention_mask'].shape)
   print(batch['labels'])
   
-  break
+  break"""
