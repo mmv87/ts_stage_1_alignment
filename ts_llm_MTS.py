94376b36706fcdc544724e3235ba0d4bfbdbf28b
mithun_vmk <mithun.kct@gmail.com>
2026-03-27 20:56:35 -0400

    small

--- ts_llm_MTS.py
+++ ts_llm_MTS.py
@@ -122,24 +122,24 @@ model_wrapper.to(device)
 
 ####check the gradient
 def check_ts_gradients(ts_encoder):
-    print("\n--- Gradient Flow Check: TS Encoder ---",flush=True)
+    print("\n--- Gradient Flow Check: TS Encoder ---")
     any_grad = False
     for name, param in ts_encoder.named_parameters():
         if not param.requires_grad:
-            print(f"{name}: Frozen (requires_grad=False)",flush=True)
+            print(f"{name}: Frozen (requires_grad=False)")
             continue
         if param.grad is None:
-            print(f"{name}: Grad is None (Graph Broken!)",flush=True)
+            print(f"{name}: Grad is None (Graph Broken!)")
         else:
             grad_norm = param.grad.norm().item()
-            print(f"{name}: Grad Norm = {grad_norm:.5f}",flush=True)
+            print(f"{name}: Grad Norm = {grad_norm:.6f}")
             if grad_norm > 1e-6:
                 any_grad = True
                 
     if not any_grad:
-        print("WARNING: No trainable parameters in TS Encoder received gradients.",flush=True)
+        print("WARNING: No trainable parameters in TS Encoder received gradients.")
     else:
-        print("Success: Gradients are flowing to TS Encoder.",flush=True)
+        print("Success: Gradients are flowing to TS Encoder.")
 
 ##** freeze the LLM for stage-1 training
 for p in model_wrapper.llm_model.parameters():
@@ -155,7 +155,7 @@ all_params = (list(model_wrapper.ts_encoder.parameters())+list(model_wrapper.llm
 optimizer = torch.optim.AdamW(all_params, lr=1e-5)
 epoch_losses=[]
 
-for epoch in range(2):  ##1 epochs
+for epoch in range(1):  ##1 epochs
     pbar = tqdm(dataloader, desc=f"Epoch {epoch}")
     num_batches = 0
     running_loss=0
@@ -177,7 +177,7 @@ for epoch in range(2):  ##1 epochs
         outputs,_= model_wrapper(input_ids=input_ids,ts_input=ts_input,ts_pairs=ts_pairs,ts_idx=ts_indices,text_idx=textual_indices,attention_mask=attention_mask,labels=labels_batch,)
         loss=outputs.loss
         loss.backward()  
-        print(f'batch{i} gradient done')
+        ##print(f'batch{i} gradient done')
         check_ts_gradients(model_wrapper.ts_encoder)##gradient calculation
         running_loss+=loss.item()
         num_batches+=1
