e12b1efeb2442d8ac0df165218a533d2072c12ee
mithun_vmk <mithun.kct@gmail.com>
2026-04-09 11:53:46 -0400

    add

--- ts_dataloader_.py
+++ ts_dataloader_.py
@@ -64,7 +64,7 @@ class ts_textual(Dataset):
                     except:
                         print('error in the line')
         
-        self.sliced_offset=self.byte_offset[:120000]
+        self.sliced_offset=self.byte_offset[:5000]
 
     def __len__(self):
         return len(self.sliced_offset)
